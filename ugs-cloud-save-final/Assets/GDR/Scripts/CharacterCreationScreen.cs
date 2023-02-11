b096bf0755bcccdfa2d6df5df19a0a3e69471f7c
yecats <yecats131@gmail.com>
2023-02-11 14:25:18 -0800

    Working on on click events

--- ugs-cloud-save-final/Assets/GDR/Scripts/CharacterCreationScreen.cs
+++ ugs-cloud-save-final/Assets/GDR/Scripts/CharacterCreationScreen.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterCreationScreen : MonoBehaviour
 {
+    [SerializeField]
+    private UIDocument _uiDocument;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _uiDocument.rootVisualElement.Q<Button>("Next").clicked += NextButton_Clicked;            
+        _uiDocument.rootVisualElement.Q<Button>("Next").clicked += NextButton_Clicked;            
     }
 
-    // Update is called once per frame
-    void Update()
+    private void NextButton_Clicked()
     {
-        
+        throw new System.NotImplementedException();
     }
 }
